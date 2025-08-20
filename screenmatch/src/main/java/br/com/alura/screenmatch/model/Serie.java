b0dcd89116d7ba65d38f21ea826666321cbebfd6
João Vitor <149846523+joaobrxyz@users.noreply.github.com>
2025-08-19 21:20:31 -0300

    Aula 03

--- screenmatch/src/main/java/br/com/alura/screenmatch/model/Serie.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/model/Serie.java
@@ -1,8 +1,6 @@
 package br.com.alura.screenmatch.model;
 
-import br.com.alura.screenmatch.service.ConsultaChatGPT;
 import br.com.alura.screenmatch.service.traducao.ConsultaMyMemory;
-import com.fasterxml.jackson.annotation.JsonAlias;
 
 import java.util.OptionalDouble;
 
