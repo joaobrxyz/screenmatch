15556c39988a0002fbe2fba6056a0feae3d1be1c
João Vitor <149846523+joaobrxyz@users.noreply.github.com>
2025-09-14 12:29:09 -0300

    Aula 10
    Retornando os detalhes da série

--- screenmatch/src/main/java/br/com/alura/screenmatch/service/SerieService.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/service/SerieService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -30,6 +31,15 @@ public class SerieService {
     }
 
     public List<SerieDTO> obterLancamentos() {
-        return converteDados(repository.findTop5ByOrderByEpisodiosDataLancamentoDesc());
+        return converteDados(repository.encontrarEpisodiosMaisRecentes());
+    }
+
+    public SerieDTO obterPorId(Long id) {
+        Optional<Serie> serie = repository.findById(id);
+        if (serie.isPresent()) {
+            Serie s = serie.get();
+            return new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse());
+        }
+        return null;
     }
 }
